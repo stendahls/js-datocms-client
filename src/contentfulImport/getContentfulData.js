1a3eb40fb1816efd89404021c146deb36e0b0614
Irene <i.oppo@cantierecreativo.net>
2018-10-31 16:15:42 +0100

    updated contentful-management

--- src/contentfulImport/getContentfulData.js
+++ src/contentfulImport/getContentfulData.js
@@ -2,14 +2,16 @@ import ora from 'ora';
 
 export default async (client) => {
   const spinner = ora('Downloading Contentful data structure').start();
-  const rawLocales = await client.getLocales();
+  const environments = await client.getEnvironments();
+  const environment = environments.items.find(e => e.name === 'master');
+  const rawLocales = await environment.getLocales();
   const defaultLocale = rawLocales.items.find(locale => locale.default).code;
   const locales = rawLocales.items.map(locale => locale.code);
-  const rawContentTypes = await client.getContentTypes();
+  const rawContentTypes = await environment.getContentTypes();
   const contentTypes = rawContentTypes.items;
-  const rawEntries = await client.getEntries();
+  const rawEntries = await environment.getEntries();
   const entries = rawEntries.items;
-  const rawAssets = await client.getAssets();
+  const rawAssets = await environment.getAssets();
   const assets = rawAssets.items;
   spinner.succeed();
 
